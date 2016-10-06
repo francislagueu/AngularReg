335eb9ce58c193aa444115ddb204678c068f6cf4
francislagueu <lagson6@yahoo.fr>
2016-10-06 02:07:54 -0700

    application in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import {AngularFire, AuthProviders, AuthMethods} from "angularfire2";
 
 @Component({
+//  moduleId:module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user = {};
+  isAuth = false;
+  authColor = 'warn';
+
+  constructor(public af:AngularFire){
+    this.af.auth.subscribe(
+      user=>this._changeState(user),
+      error=>console.trace(error)
+    );
+  }
+
+  private _changeState(user:any=null){
+    if(user){
+      this.isAuth = true;
+      this.authColor = 'primary';
+      this.user = this._getUserInfo(user);
+    }else{
+      this.isAuth = false;
+      this.authColor = 'warn';
+      this.user = {};
+    }
+  }
+
+  private _getUserInfo(user:any):any{
+    if(!user){
+      return {};
+    }
+    let data = user.auth.providerData[0];
+    return{
+      name:data.displayname,
+      avatar:data.photoURL,
+      email:data.email,
+      provider:data.providerId
+    };
+  }
+
+  private _getProvider(from:string){
+    switch(from){
+      case 'google': return AuthProviders.Google;
+      case 'facebook': return AuthProviders.Facebook;
+    }
+  }
+
+login(from:string){
+  this.af.auth.login({
+    provider:this._getProvider(from)
+  });
+}
+
+logout(){
+  this.af.auth.logout();
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import {BrowserModule } from "@angular/platform-browser";
+import {HttpModule } from "@angular/http";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import {MdModule} from './material.module';
+import{
+  AngularFireModule,
+  AuthMethods,
+  AuthProviders
+} from "angularfire2";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCI-uZ5IMmr9bdl2uSApFV3FGJ9zTareyM",
+    authDomain: "angularreg-57ea3.firebaseapp.com",
+    databaseURL: "https://angularreg-57ea3.firebaseio.com",
+    storageBucket: "angularreg-57ea3.appspot.com",
+    messagingSenderId: "1072512643650"
+};
+
 
 @NgModule({
   declarations: [
@@ -11,8 +26,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MdModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, {
+      //provider: AuthProviders.Google,
+      method:AuthMethods.Redirect
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
